25a5db5a84eda2c569e18524c3e2f3ec07ffc8ef
MKonos <Mihalis4566@gmail.com>
2017-04-02 14:42:27 -0500

    Added listview with planets to homescreen

--- app/src/main/java/uic/stella/edu/stella/HomeActivity.java
+++ app/src/main/java/uic/stella/edu/stella/HomeActivity.java
@@ -2,14 +2,36 @@ package uic.stella.edu.stella;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static uic.stella.edu.stella.R.id.planetList;
+
 // this is Bart's comment
 public class HomeActivity extends AppCompatActivity {
+
+    //controls
+    ListView planetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //THIS IS ME MAKING A CHANGE SO THAT I CAN COMMIT A
+        setTitle("Pick a Planet!");
+
+        planetList = (ListView) findViewById(R.id.planetList);
+
+        String[] aryPlanet = getResources().getStringArray(R.array.planets);
+
+        //populate listview
+        ArrayAdapter<String> aryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, aryPlanet);
+
+        setTitle("Planet Select");
+
+        //display populated listview
+        planetList.setAdapter(aryAdapter);
+
+
     }
 }
