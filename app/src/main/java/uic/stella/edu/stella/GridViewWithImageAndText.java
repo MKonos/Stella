e6708c960b55347266a57448fbe08d551c44ee52
bkupie <bkupie2@uic.edu>
2017-04-23 21:10:10 -0500

    Reverted grid view because it was broken

--- app/src/main/java/uic/stella/edu/stella/GridViewWithImageAndText.java
+++ app/src/main/java/uic/stella/edu/stella/GridViewWithImageAndText.java
@@ -23,74 +23,72 @@ public class GridViewWithImageAndText extends Fragment {
     //public class ListViewWithImageAnd extends AppSettings {
 
 
-        //private AppSettings app_settings;
+    //private AppSettings app_settings;
 
-        //controls
-        GridView planetListView;
+    //controls
+    GridView planetListView;
 
-        //vars
-        String planetSelected;
+    //vars
+    String planetSelected;
 
-        // Array of strings for ListView Title
-        String[] gridviewTitle = new String[]{
-                "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune",
-        };
+    // Array of strings for ListView Title
+    String[] gridviewTitle = new String[]{
+            "Mercury", "Venus", "Earth", "Mars",
+            "Jupiter", "Saturn", "Uranus", "Neptune",
+    };
 
 
-        int[] gridviewImage = new int[]{
-                R.drawable.mercury, R.drawable.venus, R.drawable.earth, R.drawable.mars, R.drawable.jupiter,
-                R.drawable.saturn, R.drawable.uranus, R.drawable.neptune
-        };
+    int[] gridviewImage = new int[]{
+            R.drawable.mercury, R.drawable.venus, R.drawable.earth, R.drawable.mars, R.drawable.jupiter,
+            R.drawable.saturn, R.drawable.uranus, R.drawable.neptune
+    };
 
-        String[] gridviewShortDescription = new String[]{
-                "1", "2", "3", "4",
-                "5", "6", "7", "8",
-        };
+    String[] gridviewShortDescription = new String[]{
+            "1", "2", "3", "4",
+            "5", "6", "7", "8",
+    };
 
     private View fragmentView;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_gridview_with_image_and_text, container, false);
         List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
-        fragmentView = inflater.inflate(R.layout.activity_listview_with_image_and_text, container, false);
+        fragmentView = inflater.inflate(R.layout.activity_gridview_with_image_and_text, container, false);
 
         planetListView = (GridView) fragmentView.findViewById(R.id.customgrid);
 
         for (int i = 0; i < 8; i++) {
             HashMap<String, String> hm = new HashMap<String, String>();
-            hm.put("listview_title", gridviewTitle[i]);
-            hm.put("listview_discription", gridviewShortDescription[i]);
-            hm.put("listview_image", Integer.toString(gridviewImage[i]));
+            hm.put("grid_item_label", gridviewTitle[i]);
+            hm.put("grid_item_image", Integer.toString(gridviewImage[i]));
             aList.add(hm);
-
-
-            String[] from = {"listview_image", "listview_title", "listview_discription"};
-            int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
-
-            final GridView androidListView = (GridView) rootView.findViewById(R.id.customgrid);
-            androidListView.setAdapter(new SimpleAdapter(getActivity(), aList, R.layout.listview_activity, from, to));
-
-
-            androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                    planetSelected = androidListView.getItemAtPosition(position).toString();
-
-                    Intent myIntent = new Intent(view.getContext(), DisplayPlanet.class);
-                    myIntent.putExtra("thePlanet", planetSelected);
-                    startActivity(myIntent);
-                }
-            });
-            //return rootView;
         }
-        //}
 
+
+        String[] from = {"grid_item_image", "grid_item_label"};
+        int[] to = {R.id.grid_item_image, R.id.grid_item_label};
+
+        final GridView androidListView = (GridView) rootView.findViewById(R.id.customgrid);
+        androidListView.setAdapter(new SimpleAdapter(getActivity(), aList, R.layout.grid_item, from, to));
+
+
+        androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                planetSelected = androidListView.getItemAtPosition(position).toString();
+
+                Intent myIntent = new Intent(view.getContext(), DisplayPlanet.class);
+                myIntent.putExtra("thePlanet", planetSelected);
+                startActivity(myIntent);
+            }
+        });
         return rootView;
     }
+    //}
+
 }
